d728a93720726a3b36a3c91dbe3d5f3f5d7a4ed0
jlerm13 <ja.lerman@ignitionapg.com>
2025-11-26 14:43:13 -0500

    Implement placeholder for red tier templates
    Added placeholder for red tier templates in early offseason.

--- docs/js/templates/earlyOffSeason/red.js
+++ docs/js/templates/earlyOffSeason/red.js
@@ -1,1 +1,19 @@
+// RED TIER TEMPLATES - Placeholder
+// TODO: Implement red tier templates
+(function() {
+    const redTemplates = {
+        red: {
+            'early-offseason': {
+                '2day': {},
+                '3day': {},
+                '4day': {}
+            }
+        }
+    };
 
+    if (typeof loadTemplateModule === 'function') {
+        loadTemplateModule('red', redTemplates);
+    } else {
+        console.error('loadTemplateModule function not available');
+    }
+})();
