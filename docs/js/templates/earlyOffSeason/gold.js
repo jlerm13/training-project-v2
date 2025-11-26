b96eac303d774a5ed66bf2675d385a0797dca0f5
jlerm13 <ja.lerman@ignitionapg.com>
2025-11-26 14:43:30 -0500

    Update gold.js

--- docs/js/templates/earlyOffSeason/gold.js
+++ docs/js/templates/earlyOffSeason/gold.js
@@ -1,1 +1,19 @@
+// GOLD TIER TEMPLATES - Placeholder
+// TODO: Implement gold tier templates
+(function() {
+    const goldTemplates = {
+        gold: {
+            'early-offseason': {
+                '2day': {},
+                '3day': {},
+                '4day': {}
+            }
+        }
+    };
 
+    if (typeof loadTemplateModule === 'function') {
+        loadTemplateModule('gold', goldTemplates);
+    } else {
+        console.error('loadTemplateModule function not available');
+    }
+})();
