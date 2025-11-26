82b0d0e28353bcde6ecbb49b6371102ef58cd151
jlerm13 <ja.lerman@ignitionapg.com>
2025-11-26 14:43:48 -0500

    Implement blue tier templates placeholder
    Added placeholder for blue tier templates with initial structure.

--- docs/js/templates/earlyOffSeason/blue.js
+++ docs/js/templates/earlyOffSeason/blue.js
@@ -1,1 +1,19 @@
+// BLUE TIER TEMPLATES - Placeholder
+// TODO: Implement blue tier templates
+(function() {
+    const blueTemplates = {
+        blue: {
+            'early-offseason': {
+                '2day': {},
+                '3day': {},
+                '4day': {}
+            }
+        }
+    };
 
+    if (typeof loadTemplateModule === 'function') {
+        loadTemplateModule('blue', blueTemplates);
+    } else {
+        console.error('loadTemplateModule function not available');
+    }
+})();
